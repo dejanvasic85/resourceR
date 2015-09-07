abd35ee39d2779294fbd2d405c6a74a6a495c84a
TOWER\dejan.vasic <dejan.vasic@tal.com.au>
2015-09-08 09:30:37 +1000

    Switching to using bower for dependency management

--- ResourceR/App_Start/BundleConfig.cs
+++ ResourceR/App_Start/BundleConfig.cs
@@ -1,5 +1,4 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace ResourceR
 {
@@ -8,30 +7,34 @@ namespace ResourceR
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/angular")
-                .Include("~/Scripts/angular/angular.js")
-                .IncludeDirectory("~/Scripts/angular", "*.js", true)
+            // Third party scripts
+            bundles.Add(new ScriptBundle("~/bundles/depend")
+                .Include("~/Scripts/bower_components/jquery/dist/jquery.js")
+                .Include("~/Scripts/bower_components/moment/moment.js")
+                .Include("~/Scripts/bower_components/angular/angular.js")
+                .Include("~/Scripts/bower_components/angular-resource/angular-resource.js")
+                .Include("~/Scripts/bower_components/angular-moment/angular-moment.js")
+                .Include("~/Scripts/bower_components/angular-datepicker/dist/index.js")
                 .Include("~/Scripts/bower_components/ngprogress/build/ngprogress.js")
+                .Include("~/Scripts/bower_components/bootstrap/dist/js/bootstrap.js")
                 );
 
-            bundles.Add(new ScriptBundle("~/bundles/resourceApp").IncludeDirectory("~/resourceApp", "*.js", true));
+            // Our Application
+            bundles.Add(new ScriptBundle("~/bundles/resourceApp")
+                .IncludeDirectory("~/resourceApp", "*.js", true)
+                );
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.lumen.css",
-                      "~/Content/site.css",
-                      "~/Scripts/bower_components/ngprogress/ngProgress.css"));
+            // Bootstrap - needs to have specific relative path to pick up the fonts
+            bundles.Add(new StyleBundle("~/Scripts/bower_components/bootstrap/css/bootstrap")
+                .Include("~/Scripts/bower_components/bootstrap/dist/css/bootstrap.css")
+                );
+            
+            // Our Styles
+            bundles.Add(new StyleBundle("~/Content/css")
+                .Include("~/Scripts/bower_components/ngprogress/ngProgress.css")
+                .Include("~/Scripts/bower_components/angular-datepicker/dist/index.css")
+                .Include("~/Content/site.css")
+                );
         }
     }
 }
